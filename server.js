8790977d59e40c5d813bec5df13716975060c63a
evilpaper <pellburt@gmail.com>
2021-04-12 22:03:50 +0200

    Add json db and corresponding query

--- server.js
+++ server.js
@@ -1,17 +1,68 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLInputObjectType,
+} = require("graphql");
 
 const app = express();
 
-// Dummy schema to try setup
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "HelloWorld",
-    fields: () => ({
-      message: { type: GraphQLString, resolve: () => "Hello World" },
-    }),
+const authors = [
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" },
+];
+
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "A book",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
   }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Our Root query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "A list of books",
+      resolve: () => books,
+    },
+  }),
+});
+
+// Dummy schema to try setup
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: "DummyQuery",
+//     fields: () => ({
+//       message: { type: GraphQLString, resolve: () => "Hello World" },
+//     }),
+//   }),
+// });
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
 });
 
 app.use(
